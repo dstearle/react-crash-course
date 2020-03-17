4cb82ad02a78ba56cfc4bdee61ed5edcdc4c539d
dstearle <dallas.st.earle@gmail.com>
2020-03-16 23:08:23 -0700

    Generated titles from todos state onto Todos component and created new TodoItem component

---
+++ src/components/TodoItem.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class TodoItem extends Component {
+
+    // Render
+    render() {
+
+        return(
+
+            <div>
+
+                <h1>test</h1>
+
+            </div>
+
+        )
+
+    }
+
+}
+
+export default TodoItem;
