66642215cdcac5f84734c63e6d2f4f71e5477021
dstearle <dallas.st.earle@gmail.com>
2020-03-19 18:28:50 -0700

    Gave styling to the header component

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,13 +4,22 @@ function Header() {
 
     return (
 
-        <header>
+        <header style={headerStyle}>
 
             <h1>Todo List</h1>
 
         </header>
 
     )
+
+}
+
+const headerStyle = {
+
+    background: '#333',
+    color: '#fff',
+    textAlign: 'center',
+    padding: '10px'
 
 }
 
