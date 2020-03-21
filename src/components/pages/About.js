e4105bb1e659c9707d77b30bf793f2eb0c4ec6ed
dstearle <dallas.st.earle@gmail.com>
2020-03-20 22:08:04 -0700

    Created new component for the About page and 'pages' folder

---
+++ src/components/pages/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function About() {
+
+    return(
+
+        // 
+        <React.Fragment>
+
+            {/* Title */}
+            <h1>About</h1>
+
+            <p>This is the TodoList app v.1.0</p>
+
+        </React.Fragment>
+
+    )
+
+}
+
+export default About;
