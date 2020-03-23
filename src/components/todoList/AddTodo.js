783e289a2009c86b6b8cc999f3c38e672e014fa5
dstearle <dallas.st.earle@gmail.com>
2020-03-22 18:27:47 -0700

    Gave prop types to AddTodo.js

--- src/components/todoList/AddTodo.js
+++ src/components/todoList/AddTodo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 // Class
 export class AddTodo extends Component {
@@ -53,6 +54,13 @@ export class AddTodo extends Component {
 
     }
 
+}
+
+// Proptypes
+AddTodo.propTypes = {
+
+    addTodo: PropTypes.func.isRequired,
+    
 }
 
 export default AddTodo;
